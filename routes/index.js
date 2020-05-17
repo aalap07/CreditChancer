8850a2dba65217383ac165fde579741c8e04b6a7
Aalap Patel <aalappatel07@gmail.com>
2020-05-17 14:29:07 -0400

    connection error wrapping and fetching data

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,19 @@
 var express = require("express");
 var router = express.Router();
-var mongojs = require("mongojs");
 var MongoClient = require("mongodb").MongoClient;
 
 var uri =
   "mongodb://aalap:u4ENIOWM41eZJbbK@prod-shard-00-00-kxv8d.mongodb.net:27017,prod-shard-00-01-kxv8d.mongodb.net:27017,prod-shard-00-02-kxv8d.mongodb.net:27017/test?ssl=true&replicaSet=Prod-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 MongoClient.connect(uri, function (err, client) {
-  const collection = client.db("prod").collection("records");
-  listDatabases(client);
-  client.close();
+  try {
+    listDatabases(client);
+    listEntries(client, "prod", "records");
+  } catch (err) {
+    console.error(err);
+  } finally {
+    client.close();
+  }
 });
 
 router.get("/", function (req, res, next) {
@@ -22,4 +26,10 @@ async function listDatabases(client) {
   databasesList = await client.db().admin().listDatabases();
   console.log("Databases:");
   databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
+
+async function listEntries(client, dbName, collectionName) {
+  recordsList = await client.db(dbName).collection(collectionName);
+  console.log("Records:");
+  console.log(recordsList);
 }
