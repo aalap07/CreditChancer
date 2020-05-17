ccafb32aab2ba2718f83dba72eb5b0eb89009f72
Aalap Patel <aalappatel07@gmail.com>
2020-05-17 01:03:43 -0400

    initial setup, serving empty homepage

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static(path.join(__dirname, 'client')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/', index);
+
+var port = 3000;
+app.listen(port, function(){
+    console.log('Server started on port ' + port);
+});
