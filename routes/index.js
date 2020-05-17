c2545a061a74a613a330bab44765db4a1e59eba3
Aalap Patel <aalappatel07@gmail.com>
2020-05-17 14:12:29 -0400

    establish mongodb connection and add dependencies

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var mongojs = require("mongojs");
+var MongoClient = require("mongodb").MongoClient;
 
-router.get('/', function(req, res, next){
-    res.render('index.html');
+var uri =
+  "mongodb://aalap:u4ENIOWM41eZJbbK@prod-shard-00-00-kxv8d.mongodb.net:27017,prod-shard-00-01-kxv8d.mongodb.net:27017,prod-shard-00-02-kxv8d.mongodb.net:27017/test?ssl=true&replicaSet=Prod-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+MongoClient.connect(uri, function (err, client) {
+  const collection = client.db("prod").collection("records");
+  listDatabases(client);
+  client.close();
+});
+
+router.get("/", function (req, res, next) {
+  res.render("index.html");
 });
 
 module.exports = router;
+
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
