e9efba82ff9016ad149ef0a63218e2d41ddea372
Aalap Patel <aalappatel07@gmail.com>
2020-05-18 20:40:49 -0400

    clean up mongo connection, add initial graphql setup

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,20 @@ var router = express.Router();
 var MongoClient = require("mongodb").MongoClient;
 
 var uri =
-  "mongodb://aalap:u4ENIOWM41eZJbbK@prod-shard-00-00-kxv8d.mongodb.net:27017,prod-shard-00-01-kxv8d.mongodb.net:27017,prod-shard-00-02-kxv8d.mongodb.net:27017/test?ssl=true&replicaSet=Prod-shard-0&authSource=admin&retryWrites=true&w=majority";
-
-MongoClient.connect(uri, function (err, client) {
-  try {
-    listDatabases(client);
-    listEntries(client, "prod", "records");
-  } catch (err) {
-    console.error(err);
-  } finally {
-    client.close();
-  }
-});
+  "mongodb://aalap:u4ENIOWM41eZJbbK@prod-shard-00-00-kxv8d.mongodb.net:27017,prod-shard-00-01-kxv8d.mongodb.net:27017,prod-shard-00-02-kxv8d.mongodb.net:27017/cards?ssl=true&replicaSet=Prod-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 router.get("/", function (req, res, next) {
   res.render("index.html");
+  
+  MongoClient.connect(uri, { useNewUrlParser: true },  function (err, client) {
+    try {
+      const collection = client.db("cards").collection("collection");
+    } catch (err) {
+      console.error(err);
+    } finally {
+      client.close();
+    }
+  });
 });
 
 module.exports = router;
@@ -28,8 +27,8 @@ async function listDatabases(client) {
   databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
 }
 
-async function listEntries(client, dbName, collectionName) {
-  recordsList = await client.db(dbName).collection(collectionName);
-  console.log("Records:");
+async function getEntries(client, dbName, collectionName) {
+  recordsList = await client.db(dbName).collection(collectionName).find();
   console.log(recordsList);
+  return recordsList;
 }
